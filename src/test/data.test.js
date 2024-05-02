3a12b79bcc5ed2649b13dbf727954342ddfb86a0
Tobias Wright <tobias.wright@gmail.com>
2024-05-02 01:31:08 -0500

    updated list component

--- src/test/data.test.js
+++ src/test/data.test.js
@@ -1,5 +1,7 @@
 import { expect, test } from 'vitest'
-import { items } from '../components/Data.tsx'
+import { getItems } from '../components/Data';
+
+let items = getItems();
 
 test('Check item length', () => {
   expect(items.length).toBe(31)
